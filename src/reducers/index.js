e0a405aa03654ef3501067ec6f5bcbb2d765d144
Colin Vander Glas <colinvanderglas@v1020-wn-158-228.campus-dynamic.uwaterloo.ca>
2019-01-31 15:18:46 -0500

    MERM-000:v1 of comments

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,12 +2,14 @@ import { combineReducers } from "redux";
 import { connectRouter } from "connected-react-router";
 import notification from "./notification";
 import userObject from "./userObject";
+import detailedMermComments from "./detailedMermComments"
 import requestStatus from "./requestStatus";
 
 const rootReducer = combineReducers({
   notification,
   userObject,
   requestStatus,
+  detailedMermComments,
   router: connectRouter(history)
 });
 
