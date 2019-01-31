ffcc366b2414a3bb0fe0a9ccfb70ad63c32ed490
Spencer Hivert <spencer.hivert@gmail.com>
2019-01-31 08:52:52 -0500

    MERM-000: Hookup up Merm to BE

---
+++ src/actions/mermActions.js
@@ -0,0 +1,31 @@
+import * as ActionTypes from "../constants/ActionTypes";
+import * as API from "../middleware/api";
+import { history } from "../store/configureStore";
+
+export function getMerms(authToken) {
+  return dispatch => {
+    dispatch({ type: ActionTypes.GET_REQUEST_INITIATED });
+
+    API.getMerms(authToken).then(
+      response => {
+        dispatch({
+          type: ActionTypes.UPDATE_MERM_LIST,
+          value: response.data
+        });
+        dispatch({ type: ActionTypes.GET_REQUEST_SUCCESS });
+        dispatch({ type: ActionTypes.RESET_REQUEST_STATUS });
+      },
+      error => {
+        dispatch({
+          type: ActionTypes.SHOW_NOTIFICATION,
+          value: {
+            show: true,
+            type: "Error",
+            message: "Unable fetch Merms",
+            description: "Something is broken!"
+          }
+        });
+      }
+    );
+  };
+}
---
+++ src/reducers/merms.js
@@ -0,0 +1,22 @@
+import * as ActionTypes from "../constants/ActionTypes";
+
+export function getInitialState() {
+  return {
+    allMerms: []
+  };
+}
+
+const initialState = getInitialState();
+
+export default function merms(state = initialState, action) {
+  switch (action.type) {
+    case ActionTypes.UPDATE_MERM_LIST:
+      return action.value;
+    case ActionTypes.CLEAR_MERM_LIST:
+      return getInitialState();
+    default:
+      break;
+  }
+
+  return state;
+}
