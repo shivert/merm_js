8cb848c21dd0c1974fb3005f84e821bcbbd28e96
Spencer Hivert <spencer.hivert@gmail.com>
2019-02-01 10:41:15 -0500

    MERM-000: Merge Colin branch

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,13 +2,8 @@ import { combineReducers } from "redux";
 import { connectRouter } from "connected-react-router";
 import notification from "./notification";
 import userObject from "./userObject";
-import detailedMermComments from "./detailedMermComments"
 import merms from "./merms";
-<<<<<<< HEAD
 import detailedMermComments from "./detailedMermComments";
-=======
-import detailedMermComments from "./detailedMermComments"
->>>>>>> MERM-000:v1 of comments
 import requestStatus from "./requestStatus";
 import detailedMerm from "./detailedMerm";
 
@@ -19,7 +14,6 @@ const rootReducer = combineReducers({
   detailedMermComments,
   detailedMerm,
   merms,
-  detailedMermComments,
   router: connectRouter(history)
 });
 
