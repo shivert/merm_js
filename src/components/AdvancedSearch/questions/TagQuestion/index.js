4a1126b72549d27bbcf92823596052f5a4bd8010
Colin Vander Glas <colinvanderglas@Colins-MBP.lan>
2019-03-02 19:08:07 -0500

    MERM-000:initial work for advanced search components

--- src/components/AdvancedSearch/questions/TagQuestion/index.js
+++ src/components/AdvancedSearch/questions/TagQuestion/index.js
@@ -1,13 +1,35 @@
 import React from "react";
-import { Button, Modal } from "antd";
+import { Button, Modal, Select } from "antd";
 import PropTypes from "prop-types";
+
+const Option = Select.Option;
+
+const children = [];
+for (let i = 10; i < 36; i++) {
+  children.push(<Option key={i.toString(36) + i}>{i.toString(36) + i}</Option>);
+}
+
+function handleChange(value) {
+  console.log(`selected ${value}`);
+}
 
 class TagQuestion extends React.Component {
   render() {
     return (
-      <div>
+      <>
+      <div className="tag-question">
         What are the tags?
       </div>
+
+      <Select
+        placeholder="Enter a tag"
+        mode="multiple"
+        style={{ width: '50%' }}
+        onChange={handleChange}
+      >
+      {children}
+      </Select>
+      </>
     );
   }
 }
